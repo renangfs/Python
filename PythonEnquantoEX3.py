639cfeb2b4d530689387e7cf579e314540a5def2
Renan Germano <61218420+renangfs@users.noreply.github.com>
2020-11-18 16:28:20 -0300

    Update PythonEnquantoEX3.py

--- PythonEnquantoEX3.py
+++ PythonEnquantoEX3.py
@@ -5,12 +5,13 @@ print("---------------------------------")
 SOMAMENOS = 0
 SOMAMAIS = 0
 NUM = 0
-NUM = float(input("Digite um numero:..."))
+NUM = int(input("Digite um numero:..."))
 while NUM != 0:
      if NUM > 0 :
        SOMAMAIS = SOMAMAIS + 1
-     elif NUM < 0 :
+       NUM = int(input("Digite um numero:..."))
+     if NUM < 0 :
        SOMAMENOS = SOMAMENOS + 1
-     else:
-        print("A quantidade de numeros positivos é:{} e os numeros negativos é:{}".format(SOMAMAIS,SOMAMENOS)) 
-   NUM = float(input("Digite um numero:..."))
+       NUM = int(input("Digite um numero:..."))
+
+ print("A quantidade de numeros positivos é:{} e os numeros negativos é:{}".format(SOMAMAIS,SOMAMENOS))
