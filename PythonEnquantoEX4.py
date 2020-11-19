050f167752cc488a706519952fa863d0da76aa52
Renan Germano <61218420+renangfs@users.noreply.github.com>
2020-11-19 11:51:45 -0300

    Create PythonEnquantoEX4.py

---
+++ PythonEnquantoEX4.py
@@ -0,0 +1,23 @@
+
+print("---------------------------------")
+print("-------- EX numero 4 ------------")
+print("---------------------------------")
+DIG=0
+N=2
+COD = int(input("Digite o codigo do aluno:..."))
+while N != 1:
+    NT1 = int(input("Digite NOTA 1:..."))
+    NT2 = int(input("Digite Nota 2:..."))
+    NT3 = int(input("Digite Nota 3:..."))
+    MP=(4*NT1+3*NT2+3*NT3)/10 
+    print("O aluno de codigo {} tem a media ponderada de:{}".format(COD,MP)) 
+    if MP >= 6:
+      print("Aluno: APROVADO")
+      N=int(input("Para sair digite (1) e (0) para não:.."))
+
+    elif MP <=5:
+      print("Aluno: REPROVADO")
+      N=int(input("Para sair digite (1) e (0) para não:.."))
+print("---------------------------------")
+print("-------- Fim de Processo --------")
+print("---------------------------------")
