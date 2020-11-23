a4721e75a8c7c2644f3ed859e4cfabbf3ccc71a1
Renan Germano <61218420+renangfs@users.noreply.github.com>
2020-11-23 18:09:41 -0300

    Update AV2python1.py

--- AV2python1.py
+++ AV2python1.py
@@ -10,3 +10,13 @@ for S in range (0,10):
     print("{}/{}".format(A,B))
     A=A+2
     B=B+2
+    
+    
+N=int(input("Digite o Fatorial desejado (15?): . . ."))
+F=1
+C=N
+while C > 0:
+    F *= C
+    C -= 1
+print("{}".format(F))    
+    
